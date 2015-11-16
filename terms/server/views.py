a560f39e42fd91206c2975c8906fd65e7eed8626
Enrique Pérez Arnaud <enrique@cazalla.net>
2015-11-16 17:46:12 +0100

    remove almost everything

--- terms/server/views.py
+++
@@ -1,8 +0,0 @@
-from importlib import import_module
-
-
-def init_fact_views(config):
-    plugins = config('plugins')
-    plugins = [p for p in plugins.strip().split('\n') if p]
-    plugins.append('terms.server.app')
-    return [import_module(p.strip() + '.views') for p in plugins]
